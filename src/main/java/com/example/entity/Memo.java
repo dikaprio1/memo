e24d5a820875190f9cd009c6158e0bf2fa81e829
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-03-21 19:42:08 +0900

    리스트 디비 메모장

---
+++ src/main/java/com/example/entity/Memo.java
@@ -0,0 +1,18 @@
+package com.example.entity;
+
+import com.example.dto.MemoRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Memo {
+    private Long id;
+    private String title;
+    private String contents;
+
+    public void update(MemoRequestDto RequestDto){
+        this.title=RequestDto.getTitle();
+        this.contents=RequestDto.getContents();
+    }
+}
